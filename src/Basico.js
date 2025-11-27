84be5ca1fb2daee6e48b5613e161b6ac8b36e34f
Franco <franquivillalba13@gmail.com>
2025-11-27 10:09:40 -0300

    Archivo compilado del archivo ts

---
+++ src/Basico.js
@@ -0,0 +1,12 @@
+// Tipos Fundamentales
+var productName = "Laptop Lenovo";
+var productPrice = 599.99;
+// Tipo Unión
+var productId;
+productId = "ABC123";
+productId = 1001;
+var myProduct = {
+    name: "Mouse Gamer",
+    price: 3500
+};
+console.log("Producto:", myProduct);
